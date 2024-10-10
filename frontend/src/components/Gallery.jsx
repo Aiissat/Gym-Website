5fadfcd5ff71d5e02fe2c4ff1f25de96f7542531
Diallo <m.aiissatoudiallo@gmail.com>
2024-10-10 00:45:15 +0000

    first commit

---
+++ frontend/src/components/Gallery.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Gallery = () => {
+  const gallery = [
+    "/img1.jpeg",
+    "/img2.jpeg",
+    "/img3.jpeg",
+    "/img4.jpeg",
+    "/img7.jpeg",
+    "/img8.jpeg",
+    "/img5.jpeg",
+    "/img6.jpeg",
+  ];
+  return (
+    <section className="gallery">
+      <h1>BETTER BEATS BEST</h1>
+      <div className="images">
+        <div>
+          {gallery.slice(0,3).map((element, index) => (
+            <img key={index} src={element} alt="galleryImage" />
+          ))}
+        </div>
+        <div>
+          {gallery.slice(3, 6).map((element, index) => (
+            <img key={index} src={element} alt="galleryImage" />
+          ))}
+        </div>
+        <div>
+          {gallery.slice(6, 9).map((element, index) => (
+            <img key={index} src={element} alt="galleryImage" />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>ONLY'S GIRLS SPACE</p>
+    </header>
+  );
+};
+
+export default Navbar;
